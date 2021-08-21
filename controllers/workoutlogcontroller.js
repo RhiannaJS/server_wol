751bdcb3ee235afcfde2c47c76147353c3fe023f
RhiannaJS <rhianna.latshaw@gmail.com>
2021-08-20 23:32:51 -0400

    better assets

--- controllers/workoutlogcontroller.js
+++ controllers/workoutlogcontroller.js
@@ -1,4 +1,4 @@
-const { response } = require("express");
+// const { response } = require("express");
 let Express = require("express");
 let router = Express.Router();
 let validateJWT = require("../middleware/validate-jwt");
@@ -36,15 +36,16 @@ router.get("/about", (req,res)=>{
     res.send("This is the about route!")
 })
 
-// WOULD NOT WORK IN POSTMAN
-router.get("/", async (req, res)=>{
-    try{
-        const entries = await LogModel.findAll();
-        res.status(200).json(entries);
-    } catch (err) {
-        response.status(500).json({ error: err})
-    }
-});
+// // WOULD NOT WORK IN POSTMAN
+// router.get("/", async (req, res)=>{
+//     try{
+//         const entries = await LogModel.findAll();
+//         res.status(200).json(entries);
+//     } catch (err) {
+//         response.status(500).json({ error: err})
+//     }
+// });
+
 
 router.get("/", validateJWT, async(req,res)=>{
     let {id} = req.user;
@@ -60,21 +61,19 @@ router.get("/", validateJWT, async(req,res)=>{
     }
 });
 
-router.get("/:id", validateJWT, async(req,res)=>{
-    let {id} = req.user;
+router.get("/:id", validateJWT,  async(req,res)=>{
+    let {id} = req.params;
     try{
-        const userLogs = await LogModel.findAll({
-            where: {
-                id: id
-            }
+        const results = await LogModel.findAll({
+            where: {id: id}
         });
-        res.status(200).json(userLogs);
+        res.status(200).json(results);
     } catch (err) {
         res.status(500).json({error: err});
     }
 });
 
-// HOW DO I TEST THIS IN POSTMAN???
+// HOW DO I TEST THIS IN POSTMAN??? UPDATE LOG
 router.put("/:id", validateJWT, async (req, res)=>{
     const {description, definitions, results} = req.body.log;
     const logId = req.params.entryId;
