5c4b5182bd09c20544b82c5f185312ca40aa4eaa
RhiannaJS <rhianna.latshaw@gmail.com>
2021-08-22 10:08:05 -0400

    new repo

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("postgres://postgres:be57a316870d4cc0a420b6fac69a52ab@localhost:5432/workout-log");
+const sequelize = new Sequelize("postgres://postgres:" + process.env.PASSWORD +"@localhost:5432/workout-log");
 
 module.exports = sequelize;
+
